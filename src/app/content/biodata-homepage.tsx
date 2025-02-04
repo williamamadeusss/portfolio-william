4af38f1b925d8ddbbba51ac4b44871c3fe57c407
willySprout <william@sprout.co.id>
2025-02-05 00:17:33 +0700

    on progress biodata carousel

--- src/app/content/biodata-homepage.tsx
+++ src/app/content/biodata-homepage.tsx
@@ -3,21 +3,16 @@
 import Image from '@/components/ui/image'
 import React, { useEffect, useState } from 'react'
 import { motion } from "motion/react"
-import { useTime } from "motion/react"
+import { AnimatePresence } from "motion/react"
 
 function BiodataHomepage() {
     const biodataPictures = ["/images/will.jpg", "/images/will-2.jpg"]
-    const [currentPict, setCurrentPict] = useState(biodataPictures[0])
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setIndex((prevIndex) => {
-                const nextIndex = (prevIndex + 1) % biodataPictures.length; // Loop back to start after reaching the end
-                setCurrentPict(biodataPictures[nextIndex]);
-                return nextIndex;
-            });
-        }, 3000);
+            setIndex((prevIndex) => (prevIndex + 1) % biodataPictures.length);
+        }, 4000);
 
         // Cleanup the interval on component unmount
         return () => clearInterval(interval);
@@ -25,17 +20,21 @@ function BiodataHomepage() {
 
     return (
         <div className='flex gap-40 justify-center'>
-            <motion.div
-                variants={{
-                    hidden: { opacity: 0 },
-                    visible: { opacity: 1 },
-                }}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 1 }}
-            >
-                <Image src={currentPict} alt='foto will' className='w-[29rem]' />
-            </motion.div>
+            <AnimatePresence>
+                <motion.div
+                    className='w-[29rem]'
+                    key={biodataPictures[index]}
+                    variants={{
+                        hidden: { opacity: 0 },
+                        visible: { opacity: 1 },
+                    }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <Image src={biodataPictures[index]} alt='foto will' className='w-[29rem]' />
+                </motion.div>
+            </AnimatePresence>
 
             <div className='flex flex-col gap-4'>
                 <h1 className='text-5xl'>William Amadeus</h1>
