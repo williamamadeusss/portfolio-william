5f16a3b737597c946652be64f3cb67ff5467cd3c
William Amadeus <williamama34@gmail.com>
2025-04-20 23:48:59 +0700

    minor design updates

--- src/app/components/content/biodata-section.tsx
+++ src/app/components/content/biodata-section.tsx
@@ -5,19 +5,19 @@ import React, { useEffect, useState } from "react";
 
 export default function BiodataSection() {
   return (
-    <div className="flex flex-col gap-20 lg:flex-row">
-      <div className="flex flex-col gap-10 pt-10 lg:w-3/5 lg:pt-20">
-        <div className="flex h-fit flex-col gap-4 border-b-[6px] border-primary pb-10">
-          <h1 className="text-5xl font-extrabold">William Amadeus</h1>
-          <p className="text-2xl font-semibold">A Software Engineer</p>
+    <div className="flex flex-col gap-10 lg:flex-row lg:gap-20">
+      <div className="flex flex-col gap-12 pt-10 lg:w-3/5 lg:pt-20">
+        <div className="flex h-fit flex-col gap-4 border-b-[6px] border-primary pb-5">
+          <h1 className="text-6xl font-extrabold">William Amadeus</h1>
+          <p className="text-3xl font-semibold">A Software Engineer</p>
         </div>
 
-        <p className="text-xl">
-          Hi! I’m William Amadeus — a website developer at Sprout Digital Labs
-          with a strong passion for building thoughtful, user-focused web
-          experiences. I’m currently pursuing my Bachelor’s degree in Computer
-          Science at Bina Nusantara University, where I’m constantly learning
-          and sharpening my skills in front-end development and modern web
+        <p className="text-xl font-medium tracking-wide">
+          Hi! I’m William — a website developer at Sprout Digital Labs with a
+          strong passion for building thoughtful, user-focused web experiences.
+          I’m currently pursuing my Bachelor’s degree in Computer Science at
+          Bina Nusantara University, where I’m constantly learning and
+          sharpening my skills in front-end development and modern web
           technologies.
         </p>
       </div>
@@ -25,7 +25,7 @@ export default function BiodataSection() {
       <Image
         src="/images/will.jpg"
         alt="foto will"
-        className="aspect-square rounded-full object-cover lg:w-2/5"
+        className="aspect-square rounded-lg object-cover lg:w-2/5 lg:rounded-full"
       />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BiodataSection from "./components/content/biodata-section";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-40 px-6 lg:mt-12 lg:px-20">
+    <main className="flex flex-col gap-40 px-4 lg:mt-12 lg:px-20">
       <BiodataSection />
       <p>la</p>
       <p>la</p>
