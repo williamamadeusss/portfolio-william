dff34d52378b0c694cbe3c3be60a1d8010d5baa6
William Amadeus <williamama34@gmail.com>
2025-04-24 02:43:10 +0700

    change biodata section text size using tailwind classes

--- src/app/components/biodata-section.tsx
+++ src/app/components/biodata-section.tsx
@@ -5,30 +5,24 @@ import React, { useEffect, useState } from "react";
 
 export default function BiodataSection() {
   return (
-    <div className="relative flex flex-col-reverse gap-14 lg:flex-row lg:gap-20">
+    <div className="relative flex flex-col-reverse gap-14 pb-20 lg:flex-row lg:gap-20">
       {/* invisible div to setup grid layout for desktop view */}
       <div className="hidden w-[55%] lg:block" />
 
       {/* biography section */}
       <div className="flex flex-col gap-16 px-4 lg:absolute lg:w-[65%] lg:px-0 lg:pl-20 lg:pt-32">
-        <h1 className="w-full border-b-[6px] border-primary pb-4 text-[28px] font-bold tracking-wider lg:pb-6 lg:text-left lg:text-[40px] lg:font-semibold">
+        <h1 className="text-title w-full border-b-[6px] border-primary pb-4 font-bold tracking-wider lg:pb-6 lg:text-left lg:text-heading lg:font-semibold">
           WILLIAM AMADEUS
         </h1>
 
         <div className="flex w-full flex-col items-end justify-end gap-10 lg:gap-8">
-          <h2 className="flex text-right text-5xl font-extrabold tracking-wide lg:text-7xl">
+          <p className="text-mediumHeading flex text-right font-extrabold lg:text-bigHeading">
             SOFTWARE ENGINEER
-          </h2>
-          <h2 className="flex text-right text-5xl font-extrabold tracking-wide lg:text-7xl">
+          </p>
+          <p className="text-mediumHeading flex text-right font-extrabold lg:text-bigHeading">
             WEB DEVELOPER
-          </h2>
+          </p>
         </div>
-
-        {/* <p className="text-xl font-medium tracking-wide">
-          Hi! I’m William — a frontend web developer passionate about crafting
-          smooth, user-friendly digital experiences using modern web
-          technologies.
-        </p> */}
       </div>
 
       <Image
