33b7b3b207f1ef124fe6d331b538d42824f9e77e
William Amadeus <williamama34@gmail.com>
2025-04-24 13:56:19 +0700

    on progress create biodata section

--- src/app/components/about-section.tsx
+++ src/app/components/about-section.tsx
@@ -5,32 +5,40 @@ export default function AboutSection() {
   return (
     <div
       id="about"
-      className="flex flex-col-reverse gap-14 bg-background text-backgroundBlack lg:max-h-[87vh] lg:flex-row lg:gap-20"
+      className="flex flex-col-reverse bg-background text-backgroundBlack lg:flex-row"
     >
       <Image
         src="/images/will.jpg"
         alt="about will"
-        className="w-2/5 object-cover"
+        className="h-fit w-[45%] object-cover"
       />
-      <div className="flex w-3/5 flex-col gap-10 px-4 lg:px-0 lg:pr-20 lg:pt-12">
-        <h2 className="text-mediumHeading flex w-full justify-end border-b-[6px] border-primary pb-6 font-extrabold">
+
+      <div className="flex w-[55%] flex-col gap-10 px-4 lg:px-20 lg:pt-12">
+        <h2 className="flex w-full justify-end border-b-[6px] border-primary pb-6 text-mediumHeading font-extrabold">
           about WILLIAM
         </h2>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque fugiat
-          fuga inventore est quam ducimus. Assumenda, odit numquam nostrum vel,
-          non modi repudiandae atque, labore sunt minus rerum et autem. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Totam fugiat optio
-          animi cupiditate, qui quisquam libero esse exercitationem quia
-          mollitia tempora dignissimos velit unde. Necessitatibus quidem
-          distinctio doloribus deserunt dolores! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Quos enim voluptatum alias eius hic ipsa
-          laudantium molestiae ipsum? Deserunt expedita, excepturi facere
-          dolorem quod commodi quia id dignissimos soluta reiciendis? Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Veniam est, eius a
-          eos ad soluta quo saepe sit nobis, tempore itaque nemo beatae!
-          Suscipit at totam laboriosam nobis delectus culpa?
+        <p className="text-2xl">
+          I’m William Amadeus, a Frontend-Focused Software Engineer dedicated to
+          building scalable, performant, and user-friendly web applications.
+          With a deep understanding of Next.js and Tailwind CSS, I specialize in
+          turning complex design systems into clean, accessible, and
+          maintainable codebases.
+          <br />
+          <br />
+          My technical strengths lie in translating product goals and UI/UX
+          designs into robust web experiences. I thrive in fast-paced
+          environments where attention to detail and performance matter, and I’m
+          constantly sharpening my skills to stay aligned with industry best
+          practices.
+          <br />
+          <br />
+          What drives me most is the challenge of solving complex
+          problems—whether it’s bringing ambitious UI designs to life,
+          fine-tuning performance, or diving into new technologies. I’m always
+          eager to push beyond my comfort zone and continuously grow as a
+          developer, exploring new areas of specialization and expanding my
+          capabilities with each project.
         </p>
       </div>
     </div>
