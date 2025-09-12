ebc1c0247c9b3561e2883d0cd5e24ab45945e673
William Amadeus <williamama34@gmail.com>
2025-09-12 10:12:30 +0700

    set hover effect for project card

--- src/app/components/projects-section.tsx
+++ src/app/components/projects-section.tsx
@@ -1,41 +1,57 @@
+"use client";
+
 import Image from "@/components/ui/image";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface Project {
-  name: string;
-  description: string;
+  title: string;
+  projectSlug: string;
+  shortDescription: string;
   url: string;
-  image: string;
+  techStacks: string[];
+  thumbnail: string;
 }
 
-const projects: Project[] = [
+const projects = [
   {
-    name: "Indramas",
-    description: "Description 1",
+    title: "Indramas",
+    projectSlug: "indramas",
+    shortDescription: "shortDescription 1",
     url: "https://indramas.flonkin.com/",
-    image: "",
+    techStacks: [],
+    thumbnail: "",
   },
   {
-    name: "Personal Website",
-    description: "Description 2",
+    title: "Personal Website",
+    projectSlug: "personal-website",
+    shortDescription: "shortDescription 2",
     url: "https://indramas.flonkin.com/",
-    image: "",
+    techStacks: [],
+    thumbnail: "",
   },
   {
-    name: "Stokku",
-    description: "Description 1",
+    title: "Stokku",
+    projectSlug: "stokku",
+    shortDescription: "shortDescription 1",
     url: "https://stokku.netlify.app/",
-    image: "",
+    techStacks: [],
+    thumbnail: "",
   },
   {
-    name: "GAEA",
-    description: "Description 2",
+    title: "GAEA",
+    projectSlug: "gaea",
+    shortDescription: "shortDescription 2",
     url: "https://gaea.co.id/",
-    image: "/images/projects/homepage-gaea.png",
+    techStacks: [],
+    thumbnail: "/images/projects/homepage-gaea.png",
   },
 ];
 
 export default function ProjectsSection() {
+  let [hoveredCard, setHoveredCard] = useState<string | null>(null);
+
   return (
     <div
       id="projects"
@@ -44,28 +60,68 @@ export default function ProjectsSection() {
       <h2 className="text-center font-montserrat text-mediumHeading font-extrabold tracking-wide">
         PROJECTS
       </h2>
-      <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
         {projects.map((project) => (
-          <ProjectCard key={project.name} project={project} />
+          <ProjectCard
+            key={project.title}
+            project={project}
+            hoveredCard={hoveredCard}
+            setHoveredCard={setHoveredCard}
+          />
         ))}
       </div>
     </div>
   );
 }
 
-function ProjectCard({ project }: { project: Project }) {
+function ProjectCard({
+  project,
+  hoveredCard,
+  setHoveredCard,
+}: {
+  project: Project;
+  hoveredCard: string | null;
+  setHoveredCard: (data: string | null) => void;
+}) {
   return (
-    <div className="flex flex-col gap-4">
-      <h3 className="text-smallHeading font-bold text-background">
-        {project.name}
-      </h3>
-
+    <Link
+      className={cn(
+        "relative flex flex-col gap-4 rounded-lg transition-all duration-300 ease-out",
+        hoveredCard !== null &&
+          hoveredCard !== project.projectSlug &&
+          "scale-[0.99] blur-sm",
+      )}
+      href={`/${project.projectSlug}`}
+      onMouseEnter={() => setHoveredCard(project.projectSlug)}
+      onMouseLeave={() => setHoveredCard(null)}
+    >
+      {/* image background */}
       <Image
-        src={project.image}
-        alt={project.name}
+        src={project.thumbnail}
+        alt={project.title}
         className="aspect-video w-full rounded-lg object-cover"
       />
-      <p className="">{project.description}</p>
-    </div>
+
+      {/* dark overlay */}
+      <div
+        className={cn(
+          "absolute inset-0 rounded-lg bg-black/80 transition-opacity duration-300",
+          hoveredCard === project.projectSlug ? "opacity-100" : "opacity-0",
+        )}
+      />
+
+      <div
+        className={cn(
+          "absolute flex h-full w-full flex-col justify-end gap-3 p-8 transition-opacity duration-300",
+          hoveredCard === project.projectSlug ? "opacity-100" : "opacity-0",
+        )}
+      >
+        <h3 className="text-smallHeading font-bold text-background">
+          {project.title}
+        </h3>
+
+        <p className="">{project.shortDescription}</p>
+      </div>
+    </Link>
   );
 }
